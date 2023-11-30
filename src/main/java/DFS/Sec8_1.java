57f2ffb0bfedbb440eef6be3ab5e0f18092a8f20
kun <sus5330@icloud.com>
2023-11-30 17:26:21 +0900

    풀이 추가

---
+++ src/main/java/DFS/Sec8_1.java
@@ -0,0 +1,71 @@
+package DFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+// 합이 같은 부분 집합
+public class Sec8_1 {
+
+	static boolean ch = false;
+	static int[] check = new int[10000];
+	static int target;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		solution();
+
+		if (ch) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+
+	}
+
+	static private void solution() {
+		if (Arrays.stream(arr).sum() % 2 != 0) return;
+
+		target = Arrays.stream(arr).sum() / 2;
+
+		DFS(0, 0);
+	}
+
+	/**
+	 * 1. 어떻게 마지막 요소 파악??
+	 *      반절 보다 크거나 정답 이어도 한번에 빠져나 올 수 있나?
+	 *
+	 */
+	private static void DFS(int index, int sum) {
+
+		if (ch) return;
+		if (index == arr.length) return;
+		if (sum == target) {
+			ch = true;
+			return;
+		}
+		if (sum > target) return;
+
+		// 더한 경우
+		DFS(index + 1, sum + arr[index]);
+		// 안 더한 경우
+		DFS(index + 1, sum);
+	}
+}
+
+
---
+++ src/main/java/DFS/Sec8_2.java
@@ -0,0 +1,55 @@
+package DFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 바둑이 구하기 문제
+public class Sec8_2 {
+
+	static int target;
+	static int[] arr;
+	static int max = Integer.MIN_VALUE;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int max = Integer.parseInt(st.nextToken());
+
+		int N = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		System.out.println(solution(max));
+	}
+
+	private static int solution(int m) {
+		target = m;
+
+		DFS(0, 0);
+
+		return max;
+	}
+
+	private static void DFS(int index, int sum) {
+
+		// 더한 값이 최댓값을 넘으면 리턴
+		if (sum > target) return;
+		else {
+			max = Math.max(max, sum);
+			if (index == arr.length) return;
+		}
+
+		// 더하는 경우와 안 더하는 경우 두가지.
+		DFS(index + 1, sum + arr[index]);
+		DFS(index + 1, sum);
+	}
+}

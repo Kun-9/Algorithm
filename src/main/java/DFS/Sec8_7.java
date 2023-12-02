e93ec2385d8bb15ae76669fd6b7a67482fdfd7f2
kun <sus5330@icloud.com>
2023-12-02 21:02:23 +0900

    풀이 추가

---
+++ src/main/java/DFS/Sec8_7.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	// 8_7
+
+	static int[][] dy = new int[35][35];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+
+		int n = Integer.parseInt(st1.nextToken());
+		int r = Integer.parseInt(st1.nextToken());
+
+		System.out.println(solution(n, r));
+	}
+
+	private static int solution(int n, int r) {
+		return DFS(n, r);
+	}
+
+	private static int DFS(int n, int r) {
+
+		if (dy[n][r] != 0) {
+			return dy[n][r];
+		}
+
+		if (r == n) return 1;
+		if (r == 1) return n;
+
+		return dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r);
+	}
+}

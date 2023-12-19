18c72dc90bc998ba62ad38b8d6e65fee570b7e8b
kun <sus5330@icloud.com>
2023-12-19 19:31:33 +0900

    풀이 추가

---
+++ src/main/java/lecture/Sec9_3_2.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 회의실 배정
+public class Main {
+
+	static class Person implements Comparable<Person> {
+		int time;
+		int type;
+
+		public Person(int time, int type) {
+			this.time = time;
+			this.type = type;
+		}
+
+		@Override
+		public int compareTo(Person person) {
+			int result = time - person.time;
+			if (time == person.time) {
+				result = type - person.type;
+			}
+			return result;
+		}
+
+		@Override
+		public String toString() {
+			return "Person{" +
+					"time=" + time +
+					", type='" + type + '\'' +
+					'}';
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		List<Person> personList = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int startTime = Integer.parseInt(st.nextToken());
+			int endTime = Integer.parseInt(st.nextToken());
+
+			// 1은 들어온것, 0은 나간것
+			personList.add(new Person(startTime, 1));
+			personList.add(new Person(endTime, 0));
+		}
+
+		Collections.sort(personList);
+
+
+		int cnt = 0;
+		int answer = 0;
+
+		for (Person person : personList) {
+			int time = person.time;
+			int type = person.type;
+
+			if (type == 1) {
+				cnt++;
+			} else {
+				cnt--;
+			}
+
+			answer = Math.max(cnt, answer);
+		}
+
+		System.out.println(answer);
+	}
+}
+

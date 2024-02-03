db58cd164f1fa86f1007cd318ea233bd4187ed82
kun <sus5330@icloud.com>
2024-02-03 20:37:37 +0900

    풀이 추가

--- src/main/java/BOJ/Boj_17451.java
+++ src/main/java/BOJ/Boj_17451.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package BOJ;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_17451 {
 	static int n;
 	static int[] arr;
 	public static void main(String[] args) throws IOException{

147ee76b97defe0cc76d55700dc6576fab8f12a8
kun <sus5330@icloud.com>
2024-02-12 23:49:58 +0900

    풀이 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,15 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		long[] arr = new long[N+1];
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n+1];
 
-		arr[0] = 0;
+		arr[0] = 1;
 		arr[1] = 1;
-
-		for(int i=2; i<=N; i++) {
-			arr[i] = arr[i-1] + arr[i-2];
+		for (int i = 2; i < n + 1; i++) {
+			arr[i] = (arr[i - 1] + 2 * arr[i - 2]) % 10007;
 		}
-		System.out.println(arr[N]);
 
+		System.out.println(arr[n]);
 	}
 }

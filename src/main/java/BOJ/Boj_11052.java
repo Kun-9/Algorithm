06407ff09deb1f6983d025ecda2060543b95b26a
kun <sus5330@icloud.com>
2024-02-23 23:45:22 +0900

    풀이 추가

--- src/main/java/BOJ/Boj_11052.java
+++ src/main/java/BOJ/Boj_11052.java
@@ -1,7 +1,11 @@
-import java.io.*;
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj_11052 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());

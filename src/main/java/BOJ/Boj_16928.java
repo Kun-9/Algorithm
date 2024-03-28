ea6f85ce0608c9e82a1593f2a84cfedd592ebc58
kun <sus5330@icloud.com>
2024-03-28 23:40:45 +0900

    풀이 추가

--- src/main/java/BOJ/Boj_16928.java
+++ src/main/java/BOJ/Boj_16928.java
@@ -1,9 +1,13 @@
-import java.io.*;
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj_16928 {
 
 	public static int[] map = new int[101];
 

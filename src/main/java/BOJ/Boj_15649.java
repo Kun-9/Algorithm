b1699f985953cc9af93cd39f39bded4541fc2963
kun <sus5330@icloud.com>
2024-04-10 22:32:59 +0900

    풀이 추가

--- src/main/java/BOJ/Boj_15649.java
+++ src/main/java/BOJ/Boj_15649.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package BOJ;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_15649 {
 
 	static int M;
 	static int N;
